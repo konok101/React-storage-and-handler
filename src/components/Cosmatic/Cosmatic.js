32f447f8ae630af1af67717992376f299d504b33
konok101 <rockykonok@gmail.com>
2022-05-22 03:11:37 +0600

    data load use event handle

--- src/components/Cosmatic/Cosmatic.js
+++ src/components/Cosmatic/Cosmatic.js
@@ -3,10 +3,17 @@ import './cosmatic.css'
 
 const Cosmatic = (props) => {
     const {name, id}=props.cosmatic;
+
+    const addToCart=(name)=>{
+        console.log('added', name)
+    }
+
+    const addToCartParam= ()=>addToCart(name);
     return (
         <div className='cosmatic'>
             <h3>{name}</h3>
             <h5>{id}</h5>
+            <button onClick={ addToCartParam}>Add to button</button>
         </div>
     );
 };
